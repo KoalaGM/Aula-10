ce4fc351a8dfc93e1e742996b699bd91629b7f1d
ANALISTA DE DADOS - BIG DATA SCIENCE 2025.1 <51134552025.1@senacrjedu.local>
2025-05-13 20:26:38 -0300

    aula 10

---
+++ atividade01.py
@@ -0,0 +1,25 @@
+def calculadora():
+    print("Operações: + - * /")
+    op = input("Escolha uma operação: ")
+
+    try:
+        x = float(input("Primeiro número: "))
+        y = float(input("Segundo número: "))
+
+        if op == '+':
+            print("Resultado:", x + y)
+        elif op == '-':
+            print("Resultado:", x - y)
+        elif op == '*':
+            print("Resultado:", x * y)
+        elif op == '/':
+            if y != 0:
+                print("Resultado:", x / y)
+            else:
+                print("Erro: divisão por zero")
+
+    except ValueError:
+        print("Erro: por favor, insira números válidos.")
+
+
+calculadora()
